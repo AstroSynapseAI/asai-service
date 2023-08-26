3101d28725b872c926a512860204951b49e639e9
Simun Strukan <simun.strukan@gmail.com>
2023-08-27 01:02:28 +0200

    Debugging...

--- chains/asai_chain.go
+++ chains/asai_chain.go
@@ -67,7 +67,6 @@ func (chain AsaiChain) SetSessionID(id string) {
 }
 
 func (chain AsaiChain) Run(ctx context.Context, input string) (string, error) {
-	fmt.Print("Running Asai Chain...")
 	// create llm handle
 	llm, err := openai.NewChat(
 		openai.WithModel("gpt-4"),
