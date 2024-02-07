7cf6b957165a13bbe2b1f0f5143ba768c6ec13c0
Šimun Strukan <simun.strukan@gmail.com>
2024-02-07 19:29:49 +0100

    Connectin admin chat/home view screen

--- web/src/views/admin/HomeView.vue
+++ web/src/views/admin/HomeView.vue
@@ -1,13 +1,51 @@
 <script setup>
 import PromptInput from '@/components/admin/PromptInput.vue';
-import { onMounted } from 'vue';
+import { onMounted, watch, ref, computed } from 'vue';
+import { useRoute } from 'vue-router';
 import { storeToRefs } from 'pinia';
 import { useChatStore } from '@/stores/chat.store';
+import { useUserStore } from '@/stores/user.store';
+import MarkdownIt from 'markdown-it';
+
+const route = useRoute();
 
 const chatStore = useChatStore();
-const { messages } = storeToRefs(chatStore);
+const { messages } = storeToRefs(chatStore, "messages");
 
-onMounted(() => {
+const user = useUserStore();
+
+const conversationContainer = ref(null);
+const promptContainer = ref(null);
+
+const md = new MarkdownIt({breaks: true});
+const lastMessageIndex = computed(() => messages.value.length - 1)
+const lastMessageText = computed(() => messages.value[lastMessageIndex.value]?.content || "")
+
+async function scrollToBottom() {
+  requestAnimationFrame(() => {
+    if (conversationContainer.value) {
+      var offset = promptContainer.value.offsetHeight + 30;
+      var scrollTo = conversationContainer.value.scrollHeight + offset
+      conversationContainer.value.scrollTop = scrollTo
+    }
+  });
+}
+
+watch(messages, () => {
+  scrollToBottom();
+});
+
+watch(lastMessageText, () => {
+  scrollToBottom();
+});
+
+onMounted(async () => {
+  try {
+    await user.getPrivateSession(route.params.avatar_id);
+  }
+  catch (error) {
+    console.log(error);
+  }
   feather.replace();
 });
 
@@ -21,7 +59,8 @@ onMounted(() => {
         <div class="card">
           <div class="card-body">
             <div class="container p-4 d-flex flex-column">
-              <div class="conversation-container flex-grow-1">
+              
+              <div ref="conversationContainer" class="conversation-container container-fluid flex-grow-1 overflow-auto">
                 
                 <template v-if="messages.length > 0">
                 
@@ -32,8 +71,13 @@ onMounted(() => {
                       <img src="../assets/user.svg" class="logo" alt="User Icon" width="35" height="50" v-if="message.type === 'user'"/>
                     </div>
                   
-                    <div class="col-11">  
-                      <p class="message-content">{{ message.text }}</p>
+                    <div class="col-11">
+                      <div v-if="message.isLoading">
+                        <p><span class="me-3">I'm thinking...  </span><span class="spinner mb-2 me-2"><img v-if="!message.isAgentRunnig" src="../assets/loader.png" alt=""></span></p>
+                        <p v-if="message.isAgentRunnig"><span class="me-3 fst-italic">Activating agent: {{ message.agentName }}...  </span><span class="spinner mb-2 me-2"><img src="../assets/loader.png" alt=""></span></p>
+                      </div>
+                      
+                      <div v-else class="message-content pe-3" v-html="md.render(message.content.trim())"></div>
                     </div>
                     
                     <hr class="separator opacity-100" v-if="messages.length > 1 && index !== messages.length - 1">
@@ -44,12 +88,13 @@ onMounted(() => {
 
               </div>
             
-              <div class="prompt-container">
+              <div ref="promptContainer" class="prompt-container">
                 <hr class="border border-3 opacity-100">
                 <PromptInput />
               </div>
-            </div>
 
+            
+            </div>
           </div>
         </div>
       </div>
