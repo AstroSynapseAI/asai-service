0fbcf187bc07e531af17b72d1858f766cb0046d7
cavaevinci <ivanevacicweb+private@gmail.com>
2024-02-28 12:24:44 +0100

    added toastification

--- web/src/main.js
+++ web/src/main.js
@@ -5,9 +5,13 @@ import router from './router'
 import './style.css'
 import App from './App.vue'
 
+import Toast from "vue-toastification";
+import "vue-toastification/dist/index.css";
+
 const app = createApp(App)
 const pinia = createPinia()
 
 app.use(pinia)
 app.use(router)
+app.use(Toast)
 app.mount('#app')
