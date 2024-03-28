c099c9ef3097588fbca585996aeb5c6d6eb688ce
Simun Strukan <simun.strukan@gmail.com>
2024-03-28 21:00:28 +0100

    fixed...

--- web/src/views/admin/agents/SearchAgent.vue
+++ web/src/views/admin/agents/SearchAgent.vue
@@ -5,6 +5,9 @@ import { useAgentStore } from '@/stores/agent.store';
 import { useLLMStore } from '@/stores/llm.store';
 import { useRoute } from 'vue-router';
 import { useAvatarStore } from '@/stores/avatar.store';
+import { useToast } from 'vue-toastification';
+
+const toast = useToast();
 
 const avatar = useAvatarStore();
 const agent = useAgentStore();
@@ -27,21 +30,22 @@ const config = ref({
 });
 
 const toggleSearchEngine = (id) => {
-  
+  console.log(id)
+
   if (id === 'ddg') {
-    config.ddg_is_active = !config.ddg_is_active
+    config.value.ddg_is_active = !config.value.ddg_is_active
   }
-  
+
   if (id === 'google') {
-    config.google_is_active = !config.google_is_active
+    config.value.google_is_active = !config.value.google_is_active
   }
 
   if (id === 'exa') {
-    config.exa_is_active = !config.exa_is_active
+    config.value.exa_is_active = !config.value.exa_is_active
   }
 }
 
- const submitForm = async () => {
+const submitForm = async () => {
   const formData = {
     ID: parseInt(route.params.active_agent_id),
     avatar_id: parseInt(route.params.avatar_id),
@@ -50,7 +54,7 @@ const toggleSearchEngine = (id) => {
     primer: agentPrimer.value,
     is_active: isActiveAgent.value,
     is_public: isPublicAgent.value,
-    config: JSON.stringify(config.value), 
+    config: JSON.stringify(config.value),
   }
 
   if (agentRecord.value.ID) {
@@ -59,8 +63,10 @@ const toggleSearchEngine = (id) => {
 
   try {
     await agent.saveActiveAgent(formData);
+    toast.success("Agent saved successfully");
   }
   catch (error) {
+    toast.error("Error while saving agent");
     console.log(error);
   }
 }
@@ -84,7 +90,7 @@ onMounted(async () => {
         config.value = JSON.parse(avatar.activeAgent.config);
       }
     }
-  } 
+  }
   catch (error) {
     console.log(error);
   }
@@ -93,20 +99,22 @@ onMounted(async () => {
 });
 </script>
 
- 
+
 <template>
   <h1 class="h3 mb-3">Configure: {{ agentRecord.name }}
     <div class="form-check form-switch float-end me-5">
       <label class="form-check-label" for="flexSwitchCheckDefault">Active</label>
-      <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault" :checked="isActiveAgent" @click="isActiveAgent = !isActiveAgent">
+      <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault" :checked="isActiveAgent"
+        @click="isActiveAgent = !isActiveAgent">
     </div>
     <div class="form-check form-switch float-end me-3">
       <label class="form-check-label" for="flexSwitchCheckDefault">Public</label>
-      <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault" :checked="isPublicAgent" @click="isPublicAgent = !isPublicAgent">
+      <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault" :checked="isPublicAgent"
+        @click="isPublicAgent = !isPublicAgent">
     </div>
   </h1>
 
-  
+
   <div class="card">
     <div class="card-body">
       <div class="container-fluid">
@@ -114,23 +122,17 @@ onMounted(async () => {
         <div class="row">
 
           <div class="col-6">
-            <h3>Primer</h3>
-            <Field
-              v-model="agentPrimer"
-              name="agent_primer"
-              type="text"
-              as="textarea"
-              class="form-control"
-              rows="30"
-              placeholder="How should the agent behave..."
-            ></Field>
+            <h3 class="mb-3">Primer</h3>
+            <Field v-model="agentPrimer" name="agent_primer" type="text" as="textarea" class="form-control" rows="30"
+              placeholder="How should the agent behave..."></Field>
           </div>
 
           <div class="col-6">
-            
+
             <div class="row">
               <div class="col-12">
-                <h3>Agent Model</h3>
+                <h3 class="mb-3">Agent Model</h3>
+                <h5> LLM </h5>
                 <select v-model="activeAgentLLMID" class="form-select model-select" aria-label="Select Model">
                   <option value="" disabled selected>Select a LLM</option>
                   <option v-for="(llm, index) in llmRecords" :value="llm.ID" :key="index">
@@ -142,41 +144,45 @@ onMounted(async () => {
 
             <div class="row">
               <div clas="col-12">
-                <h3>Search Tools</h3>
-                <span>Duck Duck Go</span>
-                <div class="form-check form-switch">
-                  <input class="form-check-input" type="checkbox" id="ddg-checkbox" @click="toggleSearchEngine('ddg')" :checked="config.ddg_is_active">
+                <h3 class="my-3">Search Tools</h3>
+                <div class="form-check form-switch mb-3">
+                  <span>Duck Duck Go</span>
+                  <input class="form-check-input" type="checkbox" id="ddg-checkbox" @click="toggleSearchEngine('ddg')"
+                    :checked="config.ddg_is_active">
                 </div>
 
-                <span> Google Search</span>
-                <div class="form-floating mb-3">
-                  <input type="text" class="form-control" id="apiToken" placeholder="Tool Token" v-model="config.google_api_token">
-                  <label for="apiToken">Api Token</label>
-                </div>
                 <div class="form-check form-switch">
-                  <input class="form-check-input" type="checkbox" id="google-checkbox" @click="toggleSearchEngine('google')" :checked="config.google_is_active">
+                  <input class="form-check-input" type="checkbox" id="google-checkbox"
+                    @click="toggleSearchEngine('google')" :checked="config.google_is_active">
+                  <span> Google Search</span>
+                </div>
+                <div class="form-floating mb-3">
+                  <input type="text" class="form-control" id="apiToken" placeholder="Tool Token"
+                    v-model="config.google_api_token">
+                  <label for="apiToken">Api Token</label>
                 </div>
 
-                <span> Exa Search</span>
-                
-                <div class="form-floating mb-3">
-                  <input type="text" class="form-control" id="apiToken" placeholder="Tool Token" v-model="config.exa_api_token">
-                  <label for="apiToken">Api Token</label>
-                </div>
                 <div class="form-check form-switch">
-                  <input class="form-check-input" type="checkbox" id="exa-checkbox" @click="toggleSearchEngine('exa')" :checked="config.exa_is_active">
+                  <input class="form-check-input" type="checkbox" id="exa-checkbox" @click="toggleSearchEngine('exa')"
+                    :checked="config.exa_is_active">
+                  <span> Exa Search</span>
+                </div>
+                <div class="form-floating mb-3">
+                  <input type="text" class="form-control" id="apiToken" placeholder="Tool Token"
+                    v-model="config.exa_api_token">
+                  <label for="apiToken">Api Token</label>
                 </div>
               </div>
             </div>
-            
+
           </div>
-          
+
         </div>
 
         <div class="row mt-3">
           <div class="col-12">
-            <button type="button" class="btn btn-secondary" @click="submitForm">Save</button>
-          </div>    
+            <button type="button" class="btn btn-primary" @click="submitForm">Save All</button>
+          </div>
         </div>
 
       </div>
@@ -184,3 +190,9 @@ onMounted(async () => {
   </div>
 
 </template>
+
+<style scoped>
+.form-control, .form-select {
+  background-color: #374151;
+}
+</style>
