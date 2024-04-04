762163f2611d7921a4c89cf071fe9dcac1c5467d
Simun Strukan <simun.strukan@gmail.com>
2024-04-04 19:14:09 +0200

    FE build and merge

--- web/static/assets/BrowserAgent-42c028dc.js
+++
@@ -1,1 +0,0 @@
-import{_ as r}from"./index-10c4e2e0.js";const e={};function n(t,c){return null}const o=r(e,[["render",n]]);export{o as default};
---
+++ web/static/assets/BrowserAgent-7133f0bd.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./index-a4c4dc87.js";const e={};function n(t,c){return null}const o=r(e,[["render",n]]);export{o as default};
